2c4c07422e9038596cde89c411559db1535a7a17
Justin Webb <webb.n.justin@gmail.com>
2020-07-15 13:58:56 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -1,1 +1,27 @@
-# Write your methods here
+def loop_message_five_times(string)
+  count = 0 
+  
+  while count < 6 
+    puts "Hello World."
+    count += 1 
+  end
+end
+
+
+def loop_message_n_times(message, input)
+  count = 0
+  while count < input do
+    puts message
+    count += 1
+  end
+end
+
+def output_array(array)
+  count = 0 
+  while count < array.length do 
+    puts array [count]
+    count += 1 
+  end
+end
+
+def return_string_aray
