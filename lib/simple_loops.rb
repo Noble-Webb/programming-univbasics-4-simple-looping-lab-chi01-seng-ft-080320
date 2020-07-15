a8c620ab44880ad2ac33d6286fa767e08a61d43a
Justin Webb <webb.n.justin@gmail.com>
2020-07-15 14:08:37 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -26,8 +26,11 @@ end
 
 def return_string_aray(array)
   count = 0 
+  new_array = []
   while count < arrary.length do
     arrary[count].to_s 
+    new_array.push(array[count].to_s)
     count += 1 
   end
+  new_array
 end
